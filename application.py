6b38dedf3bcf6a16fe8bde4e4ed55e221bc6e1e0
ShawnHe711 <sheng.he@daocloud.io>
2016-06-23 12:41:12 +0800

    Update application.py

--- application.py
+++ application.py
@@ -10,7 +10,7 @@ def hello_world():
   storage = Storage()
   storage.populate()
   score = storage.score()
-  return "Hello Shanghai, %d!" % score
+  return "Hello Shanghai DaoCloud, %d!" % score
 
 class Storage():
   def __init__(self):
